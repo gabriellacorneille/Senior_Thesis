42e4e6a1898cf8883ae6ab5578d3fbacbcd93cf6
Gabby Corneille <gabbycorneille@Mac.attlocal.net>
2025-12-31 18:37:14 -0600

    successful top10 method

--- Drafts/Draft - Preliminary Figure 1.R
+++ Drafts/Draft - Preliminary Figure 1.R
@@ -13,60 +13,45 @@ library(patchwork)
 setwd("/Users/gabbycorneille/Desktop/Senior Thesis/Senior_Thesis")
 
 #this line will load in your csv to your environment
-uasdata <- read_csv("IntermediateData/uasdata.csv")
+data <- read_csv("IntermediateData/uasdata.csv")
 
-#we need to filter out the seals that are categorized as (N & water) not complete in the p_complete column
+#there is a typo in row 10543 where it says "malfee" instead of male, this will fix that
+uasdata <- data %>%
+  mutate(age_sex = if_else(age_sex == "malfee", "male", age_sex))
+
+#fixed it
+
+#now we need to filter out the seals that are categorized as (N & water) not complete in the p_complete column
 uasdata_complete <- uasdata %>%
-  filter(is.na(p_complete) | !p_complete %in% c("N", "water"))
+  filter(
+    is.na(p_complete) | !p_complete %in% c("N", "water"),
+    age_sex !="pup")
 
-#here I am trying to categorize males above a certain length as alpha males
+
+
+#this is the original method of categorizing alphas based on length.
 #uasdata_complete$highlight_group <- with(
   uasdata_complete,
   ifelse(age_sex == "male" & length>=4, "alpha_male", age_sex)
 )#when i ran this, I realized there just weren't males equal to or above 4m in the year 2016 and only 1 in 2018 so I will now try to come up with a way of getting the alpha males to be the seals within the top 10% of length for each individual year.
 
-#here is my attempt 
+
+#here is my attempt at categorizing alphas as the top ten percent length wise based on each individual year. 
 uasdata_complete_top10 <- uasdata_complete %>%
   group_by(year) %>%
   filter(
     age_sex == "male",
-    length >= quantile(length, 0.9, na.rm = TRUE)) %>%
+    length >= quantile(length[age_sex=="male"], 0.9, na.rm = TRUE)) %>%
   ungroup()
 #now this will list out the number of males within the top 10% of length for each individual year (these will be your alphas)
 uasdata_complete_top10 %>% 
   count(year)
-#this gives an range of 108 to 247 alpha males across the years which is probably too many so let's make it the top 5 percent of lengths.
-uasdata_complete_top5 <- uasdata_complete %>%
-  group_by(year) %>%
-  filter(
-    age_sex == "male",
-    length >= quantile(length, 0.95, na.rm = TRUE)) %>%
-  ungroup()
-
-uasdata_complete_top5 %>% 
-  count(year)
-#using the top 5 percent gives us a range of 104 to 154 alpha males across individual years
 
 #now is there a way to reintegrate this info into the main dataset?
-#this one is for top 5 percent and gave me extremely low numbers for alphas per year (6-14)
-uasdata_main1 <- uasdata_complete %>%
-  group_by(year) %>%
-  mutate(
-    alpha_cutoff = quantile(length[age_sex == "male"], 0.95, na.rm = TRUE),
-    dominance_group = case_when(
-      age_sex == "female" ~ "female",
-      age_sex == "male" & length >= alpha_cutoff ~ "alpha male",
-      age_sex == "male" ~ "male",
-      age_sex == "pup" ~ "pup",
-      TRUE ~ NA_character_
-    )
-  ) %>%
-  ungroup() %>%
-  select(-alpha_cutoff)
 
 #this for top 10 percent and gives me a range of (12-27) males per year
 uasdata_main2 <- uasdata_complete %>%
-  group_by(year) %>%
+  group_by(year) %>%#this line communicates that 90th percentiles are calculated for each individual year
   mutate(
     alpha_cutoff = quantile(length[age_sex == "male"], 0.9, na.rm = TRUE),
     dominance_group = case_when(
@@ -80,9 +65,32 @@ uasdata_main2 <- uasdata_complete %>%
   ungroup() %>%
   select(-alpha_cutoff)
 
-#this tells me how many alphas there are each year  
+#this tells me how many alphas there are each year (just to check that its giving me the right amount of alphas per year) 
 uasdata_main2%>%
   filter(dominance_group == "alpha male") %>%
   count(year)
+
+
+#now we're only working with uasdata_main2
+
+#here is the start of creating the linear graph
+
+#split  data up by class
+females <- uasdata_main2 %>%
+  filter(dominance_group == "female") 
+
+alpha_males <- uasdata_main2 %>%
+  filter(dominance_group == "alpha male")
+
+#ok now we have run into a problem, this new dataset does not have location data for each seal like the dataset from 128L...
+
+
+
+
+
+
+
+
+
 
 
